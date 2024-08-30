9746d95d92712d6ba20452584ef36e146eb086d3
SamruddhiiDesai <121622397+SamruddhiiDesai@users.noreply.github.com>
2024-08-30 23:10:02 +0530

    Add files via upload

---
+++ ABC/Demo.java
@@ -0,0 +1,41 @@
+class Demo{
+   public static void main(String[] args){
+	  /*int a= 10;
+	  do{
+		  a--;
+		  System.out.println("a");
+		  
+	  }while(a>0);*/
+	  
+	 // for (int i = 0 ; i<5 ; i++){
+			//if(true){
+			//	break; //op=nothing
+			//}
+			//System.out.println("Inside error");
+//}	
+		//System.out.println("Inside error"); 
+		/*for (int i = 0 ; i<=5 ; System.out.println("incre/decre block of for loop")){
+			System.out.println("Inside for");
+			i++;
+		}*/
+		/*for (int i = 0 ; 1 ; i++){
+			System.out.println("Inside for loop");
+		}
+		for (int i = 0 ; 0 ; i++){
+			System.out.println("Inside for loop");
+		}*/
+	   
+   /*for(I=0;I<5;I++);//error
+   {
+      System.out.println("Demo"); 
+   }*/
+   int i;
+   while(i<4){
+	   System.out.println("*"+i);
+	   break;
+   }
+   
+   
+   }
+   
+}
